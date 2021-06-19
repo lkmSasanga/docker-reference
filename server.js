b3b061791a78bbc87c190dfec40964d17c94c118
lkmSasanga <lkmsasanga@gmail.com>
2021-06-19 10:14:21 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
-
-let userGoal = 'Learn Docker!';
-
-app.use(
-    bodyParser.urlencoded({
-        extended: false,
-    })
-);
 
 app.use(express.static('public'));
 
@@ -21,26 +12,11 @@ app.get('/', (req, res) => {
       </head>
       <body>
         <section>
-          <h2>My Course Goal!</h2>
-          <h3>${userGoal}</h3>
+        <h2>Docker Beginner Guide!!!</h2>
         </section>
-        <form action="/store-goal" method="POST">
-          <div class="form-control">
-            <label>Course Goal</label>
-            <input type="text" name="goal">
-          </div>
-          <button>Set Course Goal</button>
-        </form>
       </body>
     </html>
   `);
-});
-
-app.post('/store-goal', (req, res) => {
-    const enteredGoal = req.body.goal;
-    console.log(enteredGoal);
-    userGoal = enteredGoal;
-    res.redirect('/');
 });
 
 app.listen(80);
